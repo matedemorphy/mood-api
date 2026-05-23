ea0a90048041af28bf23cafe88168d8f03d83981
Joseph Palmezano <josephandres.co@gmail.com>
2026-05-23 16:00:54 -0500

    sentry logger

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,9 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.dig(:sentry, :dsn) || ENV["SENTRY_DSN"]
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
-
+  config.enable_logs = true
+  # Patch Ruby logger to forward logs
+  config.enabled_patches = [:logger]
   # Add data like request headers and IP for users,
   # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
   config.send_default_pii = true
